a1495aa62fac53f3549fd9338e433596050f561d
tam1006 <tamusyun1006@gmail.com>
2022-11-26 15:48:22 +0900

    add ABC 038 D

--- 2-3/longest-increasing-subsequence/ABC_038_D.py
+++ 2-3/longest-increasing-subsequence/ABC_038_D.py
@@ -2,17 +2,14 @@ def solve(N, h, w):
     # dp = [[float('inf'), float('inf')]] * N
     dp = [float('inf')] * N
 
-    h, w = zip(*sorted(zip(h, w), key=lambda x: max(x[0], x[1])))
+    h, w = zip(*sorted(zip(h, w), key=lambda x: (x[0], -x[1])))
 
     import bisect
     ans = 0
     for i in range(N):
-        # index = min(bisect.bisect_left(dp[:][0], w[i]), bisect.bisect_left(dp[:][1], h[i]))
-        index = bisect.bisect_left(dp, max(w[i], h[i]))
-        dp[index] = max(w[i], h[i])
+        index = bisect.bisect_left(dp, w[i])
+        dp[index] = w[i]
         ans = max(ans, index+1)
-
-        print(dp)
 
     return ans
 
