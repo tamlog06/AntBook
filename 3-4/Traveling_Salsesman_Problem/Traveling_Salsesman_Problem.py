6b66d49f2a3c575cca1fc00db464893cb8584414
tam1006 <tamusyun1006@gmail.com>
2022-12-17 17:43:29 +0900

    add Traveling Salesman Problem

---
+++ 3-4/Traveling_Salsesman_Problem/Traveling_Salsesman_Problem.py
@@ -0,0 +1,46 @@
+# Solution 1
+
+N, M  = map(int, input().split())
+edges = [[float('inf') for _ in range(N)] for _ in range(N)]
+
+for _ in range(M):
+    v, u, w = map(int, input().split())
+    edges[v][u] = w
+
+dp = [[-1 for _ in range(N)] for _ in range(1 << N)]
+
+def rec(S, v):
+    if dp[S][v] >= 0:
+        return dp[S][v]
+
+    if S == (1 << N) -1 and v == 0:
+        dp[S][v] = 0
+        return 0
+
+    res = float('inf')
+    for u in range(N):
+        if not S >> u & 1:
+            res = min(res, rec(S | 1 << u, u) + edges[v][u])
+
+    dp[S][v] = res
+    return res
+
+print(rec(0, 0))
+
+# Solution 2
+
+
+
+"""
+入力例1
+--------------
+5 8
+0 1 3
+0 3 4
+1 2 5
+2 0 4
+2 3 5
+3 4 3
+4 1 6
+4 0 7
+"""
